99b0edccde0329194cfeb757e3d2b8c2f3d2b42b
Brian LaMattina <blamattina@users.noreply.github.com>
2023-05-14 20:28:28 +0000

    Setup example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('Hello World');
+import { merge } from "./merge";
+
+console.log(merge([1, 2, 3], [4, 5, 6], 3)); // [1, 2, 3]
+
---
+++ src/merge.ts
@@ -0,0 +1,3 @@
+// Merge two sorted arrays into one sorted array with a limit
+export function merge(a: number[], b: number[], limit: number): number[] {
+}
